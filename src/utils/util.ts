a742f8ca4130ca0748876fb471cb9d3b1d9112e7
Maxus-V <1902702614@qq.com>
2023-03-27 20:23:54 +0800

    #Add GRE-14 test

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -35,7 +35,7 @@ export const getBrowserLang = () => {
 export const searchRoute = (path: string, routes: RouteObject[] = []): RouteObject => {
 	let result: RouteObject = {}
 	for (let item of routes) {
-		if (item.path === path) return item
+		if (item.path === path) return item 
 		if (item.children) {
 			const res = searchRoute(path, item.children)
 			if (Object.keys(res).length) result = res
