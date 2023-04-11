ee50b77326ba35f59d0a756347f2cc62a890ad01
Maxus-V <1902702614@qq.com>
2023-04-11 15:25:53 +0800

    #Add GRE-29 新建electron测试纯净分支

--- electron/main.js
+++ electron/main.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 let mainWindow
 
-function createWindow () {
+const createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
@@ -28,7 +28,7 @@ function createWindow () {
     // mainWindow.setResizable(false)
     // mainWindow.setFullScreen(true)
 
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed',  () => {
         mainWindow = null
     })
     
@@ -39,13 +39,13 @@ function createWindow () {
 
 app.on('ready', createWindow)
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
